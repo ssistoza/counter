d4eaac9e9bbca7331e3e57f9efe731535b532ba8
Shane Avery Sistoza <me@ssistoza.dev>
2020-10-13 08:39:34 -0400

    add reset param

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # Counter
-A Simple counter with name and hit count of api. 
+
+A simple GET counter with name and hit count of api.
 
 ## Request Parameters:
 
 ### name
-Type: string
-default: 'default'
-The name of counter, when no name is matched a new counter is made with the name and the count begins at 1.
+
+- Type: string
+- Default Value: 'default'
+  The name of counter, when no name is matched a new counter is made with the name and the count begins at 1.
 
 ### simulate
-Type: boolean
-default: false
-Add one to counter, when false and persist change.
-When true, does not persist the additional count as one.
+
+- Type: boolean
+- Default Value: false
+  Add one to count, when false and persist change.
+  When true, does not persist the additional count as one.
+
+## reset
+
+- Type: boolean
+- Default Value: false
+  Resets count back to zero if true.
--- api/counter.ts
+++ api/counter.ts
@@ -5,11 +5,12 @@ const COLLECTION = 'tools';
 const DOCUMENT = 'hit-count';
 
 const COUNTER = async (req: NowRequest, res: NowResponse) => {
-  const { name = 'default', simulate = false } = req.query;
+  const { name = 'default', simulate = false, reset = false } = req.query;
   const docRef = firebaseInstance.collection(COLLECTION).doc(DOCUMENT);
   const params = {
     name: name as string,
     simulate: simulate as boolean,
+    reset: reset as boolean,
   };
 
   const doc = await docRef.get();
@@ -19,11 +20,12 @@ const COUNTER = async (req: NowRequest, res: NowResponse) => {
   }
 
   const data = doc.data();
-  const count = data[params.name] ? data[params.name] + 1 : 1;
+  let count = data[params.name] ? data[params.name] + 1 : 1;
 
   res.status(200).send({ name: params.name, count });
-  if (!simulate) {
-    docRef.set({ [params.name]: count }, { merge: true });
+
+  if (!params.simulate) {
+    docRef.set({ [params.name]: params.reset ? 0 : count }, { merge: true });
   }
 };
 
