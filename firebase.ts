03478ed53203d956314e98135ba8dbc03a847a87
Shane Avery Sistoza <me@ssistoza.dev>
2020-10-13 07:52:26 -0400

    changed env vars

--- firebase.ts
+++ firebase.ts
@@ -4,7 +4,7 @@ try {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PPK.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     } as admin.ServiceAccount),
     databaseURL: process.env.FIREBASE_DB,
